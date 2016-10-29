02c3c8c01bc104f1c2784de7b468a6b58c0d2586
yanzhou03 <yanzhou03@gmail.com>
2016-10-29 14:32:47 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,16 @@
 ## environment under the global enrionment and defines four
 ## functions that can be accessed by subsequent command.
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL #initalize storage matrix
+  Inv<-NULL #initalize storage matrix
   
   #define set() function
   set<-function(y){
     x<<-y  #assign the input y to x object in the parent environment
-    m<<-NULL #assign the value of NULL to m object in the paraent environment
+    Inv<<-NULL #assign the value of NULL to m object in the paraent environment
   }
   get<-function() x #retrieve matrix x from parent envrionment
-  setmatrix<-function(solve) m<<- solve  #setter for the inverse
-  getmatrix<-function() m #getter for the inverse
+  setmatrix<-function(solve) Inv<<- solve  #setter for the inverse
+  getmatrix<-function() Inv #getter for the inverse
   
   #Assign each of the previous four functions as an element with a list()
   list(set=set, get=get,
@@ -29,15 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m<-x$getmatrix() #access the getmatrix() function in MakeCacheMatrix
-  if(!is.null(m)){
+  Inv<-x$getmatrix() #access the getmatrix() function in MakeCacheMatrix
+  if(!is.null(Inv)){
     message("getting cached data")
-    return(m)
+    return(Inv)
   }
-  datos<-x$get() #use get() function in MakeCasheMatrix to retrieve input matrix
-  m<-solve(datos, ...) #solve for the inverse of the input matrix
-  x$setmatrix(m) #get the inverse matrix
-  m #return the inverse matrix
+  dat<-x$get() #use get() function in MakeCasheMatrix to retrieve input matrix
+  Inv<-solve(dat, ...) #solve for the inverse of the input matrix
+  x$setmatrix(Inv) #get the inverse matrix
+  Inv #return the inverse matrix
 }
 
 #make up a matrix and try if the code works well
